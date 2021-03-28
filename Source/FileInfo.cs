a0f81152575a7600b06c29d02e781aa5a22df40d
THISACCCOUNT <46848515+THISACCCOUNT@users.noreply.github.com>
2021-03-27 22:27:05 -0500

    Update FileInfo.cs

--- Source/FileInfo.cs
+++ Source/FileInfo.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MultiplayerMod
-{
-    public static class FileInfo
-    {
-        public const string Name = "Multiplayer Mod - PVP";
-        public const string Author = "Someone Somewhere, Maranara, zCubed";
-        public const string Company = null;
-        public const string Version = "0.13.0";
-        public const string DownloadLink = "https://discord.gg/2Wn3N2P";
-    }
-
-}
+CODING CODING CODIGN WOO
